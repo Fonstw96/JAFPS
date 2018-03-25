c478d239be7f1e0712fd88bc4302b7560745179a
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-25 19:44:25 +0200

    Can see platform placement
    next up: actually place a thing!

---
+++ Assets/Scripts/PlacePlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacePlatform : MonoBehaviour
+{
+    public float fPlaceDistance = 12;
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+	}
+}
