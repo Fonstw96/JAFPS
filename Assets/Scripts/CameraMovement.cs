323f2c975ad62243fd743bd1742bfc21c8fc6b0f
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-25 19:16:38 +0200

    First person controller

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float fMouseSensitivity = 1;
+    public Transform tPlayer;
+    
+    private Vector3 vMouseRotation;
+    private Vector3 vPlayerRotation;
+
+    void Start ()
+    {
+	}
+
+	void Update ()
+    {
+        Look();
+    }
+
+    void Look()
+    {
+        // For stability
+        vMouseRotation = transform.rotation.eulerAngles;
+
+        // For readability
+        vMouseRotation.x -= Input.GetAxis("Mouse Y") * fMouseSensitivity;
+        vPlayerRotation.y += Input.GetAxis("Mouse X") * fMouseSensitivity;
+
+        // Actual rotation
+        transform.rotation = Quaternion.Euler(vMouseRotation);
+        tPlayer.rotation = Quaternion.Euler(vPlayerRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float fSpeed = .2f;
+    public float fJumpStrength = 400;
+    public float fFallSpeed = 2.5f;
+
+    private Rigidbody rb;
+
+	void Start ()
+	{
+        rb = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		Walk();
+        SmoothJump();
+	}
+
+	void Walk()
+	{
+        // For readability
+		Vector3 vForward = transform.forward * Input.GetAxis("Vertical") * fSpeed;
+		Vector3 vSideways = transform.right * Input.GetAxis("Horizontal") * fSpeed;
+		//print("F: " + vForward + ", S: " + vSideways);
+
+        // Actual movement
+		transform.position += vForward;
+		transform.position += vSideways;
+	}
+
+    void SmoothJump()
+    {
+        // For readability
+        Vector3 vJump = transform.up * 0;
+
+        if (Input.GetButtonDown("Jump"))
+            vJump = transform.up * fJumpStrength;
+
+        // Actual jump
+        rb.AddForce(vJump);
+
+        // Faster fall
+        rb.velocity += Vector3.up * Physics.gravity.y * (fFallSpeed - 1) * Time.deltaTime;
+    }
+}
