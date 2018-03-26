56ca8e692fa8122ecc1c4ba41c177b3cd2c4b7e1
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-26 12:55:29 +0200

    You can transcend worlds!

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public int iNextLevel = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (iNextLevel < 0)
+                print("Game over!");
+            else
+                SceneManager.LoadScene(iNextLevel);
+        }
+    }
+}
