cb43e4d33d5089c3e3e5026a3513d8256bb8ba35
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-26 16:33:47 +0200

    You can now set the distance of your platform-placing powers!

--- Assets/Scripts/PlacePlatform.cs
+++ Assets/Scripts/PlacePlatform.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlacePlatform : MonoBehaviour
 {
-    public float fPlaceDistance = 12;
+    public float fPlaceDistance = 8;
     public GameObject goPlatform;
+    private LineRenderer lrLaser;
 
 	void Start ()
     {
+        lrLaser = GetComponent<LineRenderer>();
 	}
 	
 	void Update ()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            fPlaceDistance += Input.mouseScrollDelta.y;
+            fPlaceDistance = Mathf.Clamp(fPlaceDistance, 4, 12);
+            lrLaser.SetPosition(1, new Vector3(0, -.4f, fPlaceDistance));
+        }
+
+            if (Input.GetButtonDown("Fire1"))
             Instantiate(goPlatform, transform.position + transform.forward * fPlaceDistance, new Quaternion());
 	}
 }
