d4f22e97ac18daad7ca0646a93d45da046a213ba
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-26 14:51:20 +0200

    You can die now!

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float fMovementSpeed = .2f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            transform.LookAt(other.transform);
+            transform.position += transform.forward * fMovementSpeed;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
