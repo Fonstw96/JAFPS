fd908930c6af46cd7c86983d90297e8ec0ffe529
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-26 15:11:14 +0200

    You can fall to you death now!
    In all honesty though, this just saves the player the annoyance of either resetting the game or accidentally encounter a crawler while trying to climb back!

---
+++ Assets/Scripts/FallingDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallingDeath : MonoBehaviour
+{
+    public float fMaxFall = 108;
+
+	void Update ()
+    {
+        if (transform.position.y < -fMaxFall)
+        {
+            if (tag == "Player")
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            else
+                Destroy(gameObject);
+        }
+	}
+}
