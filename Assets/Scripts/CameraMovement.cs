a75bb61ce1fa56b768a21f631dafa44eb85c4bc5
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-27 11:49:24 +0200

    Camera clitch fix, wall fix, setup 2nd level
    These name are getting boring

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -29,8 +29,33 @@ public class CameraMovement : MonoBehaviour
         vMouseRotation.x -= Input.GetAxis("Mouse Y") * fMouseSensitivity;
         vPlayerRotation.y += Input.GetAxis("Mouse X") * fMouseSensitivity;
 
+        // Buf fixing (pls?)
+        vMouseRotation.x = ClampAngle(vMouseRotation.x, -85, 85);
+
         // Actual rotation
         transform.rotation = Quaternion.Euler(vMouseRotation);
         tPlayer.rotation = Quaternion.Euler(vPlayerRotation);
+    }
+
+    // Jesse from my class made this: 
+
+    /// <summary>
+    /// clamp an angle between to values
+    /// </summary>
+    public static float ClampAngle(float a, float min, float max)
+    {
+        while (max < min) max += 360.0f;
+        while (a > max) a -= 360.0f;
+        while (a < min) a += 360.0f;
+
+        if (a > max)
+        {
+            if (a - (max + min) * 0.5f < 180.0f)
+                return max;
+            else
+                return min;
+        }
+        else
+            return a;
     }
 }
