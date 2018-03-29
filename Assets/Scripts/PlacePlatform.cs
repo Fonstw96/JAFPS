6711865e46ef068ef1edd706c73e49d7eb6cdb69
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-29 12:27:05 +0200

    Some fine tuning
    Added laser colour!

--- Assets/Scripts/PlacePlatform.cs
+++ Assets/Scripts/PlacePlatform.cs
@@ -14,7 +14,7 @@ public class PlacePlatform : MonoBehaviour
     void Start ()
     {
         lrLaser = GetComponent<LineRenderer>();
-	}
+    }
 	
 	void Update ()
     {
@@ -43,6 +43,27 @@ public class PlacePlatform : MonoBehaviour
                 iCurAmmo += 5;
             else if (iCurAmmo > 4)
                 iCurAmmo -= 5;
+
+            switch (iCurAmmo)
+            {
+                case 0:
+                    lrLaser.startColor = new Color(0, .25f, 1, .75f);
+                    lrLaser.endColor = new Color(0, .25f, 1, .75f);
+                    break;
+                case 1:
+                    lrLaser.startColor = new Color(.8f, 0, 0, .75f);
+                    lrLaser.endColor = new Color(.8f, 0, 0, .75f);
+                    break;
+                case 4:
+                    lrLaser.startColor = new Color(.75f, .25f, 0, .75f);
+                    lrLaser.endColor = new Color(.75f, .25f, 0, .75f);
+                    break;
+                default:
+                    lrLaser.startColor = new Color(.9333f, .9333f, 0, .75f);
+                    lrLaser.endColor = new Color(.9333f, .9333f, 0, .75f);
+                    break;
+
+            }
         }
 
         if (Input.GetButtonDown("Fire1") && iAmmo[iCurAmmo] > 0)
