a53e0429a6286f9fd395f5b6e5bbd9a3607f4d3a
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-06 14:52:04 +0200

    Fixed jumping issue
    People would try to jump off walls and enemies and they couldn't... WELL NOW YOU CAN!!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
     {
         GameObject other = collision.gameObject;
 
-        if (other.tag == "Surface")
+        if (other.tag != "NoJump")
             bAllowJump = true;
     }
 
@@ -34,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
     {
         GameObject other = collision.gameObject;
 
-        if (other.tag == "Surface")
+        if (other.tag != "NoJump")
             bAllowJump = false;
     }
 
